4318667c97c1453bbdd65738535f1396b55b4842
Rudraksh Mahajan <91263379+raiden9420@users.noreply.github.com>
2024-07-24 14:27:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+st.title('Langchain Chatbot with LLAMA 2')
+input_text=st.text_input("Chat with me...")
+
+llm = Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
